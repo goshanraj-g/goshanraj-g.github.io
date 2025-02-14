5697d6a492a39e9bb5e01e0cfa17c4a0521e2b09
Goshanraj Govindaraj <govindag@mcmaster.ca>
2025-02-14 16:39:10 -0500

    fixed formatting + theme switch

--- script/script.js
+++ script/script.js
@@ -1,5 +1,17 @@
 const themeToggle = document.getElementById('theme-toggle');
 
+const savedTheme = localStorage.getItem('theme');
+if (savedTheme === 'light') {
+    document.body.classList.add('light-theme');
+    themeToggle.checked = true;
+}
+
 themeToggle.addEventListener('change', () => {
-  document.body.classList.toggle('light-theme');
+    if (themeToggle.checked) {
+        document.body.classList.add('light-theme');
+        localStorage.setItem('theme', 'light');
+    } else {
+        document.body.classList.remove('light-theme');
+        localStorage.setItem('theme', 'dark');
+    }
 });
