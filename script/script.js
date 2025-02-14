b57968d4fb5f3a8dd697653dc0ff98309e029fef
Goshanraj Govindaraj <govindag@mcmaster.ca>
2025-02-13 23:08:46 -0500

    colorchanges

--- script/script.js
+++ script/script.js
@@ -1,7 +1,5 @@
-// Select the toggle checkbox
 const themeToggle = document.getElementById('theme-toggle');
 
-// Listen for toggle changes
 themeToggle.addEventListener('change', () => {
   document.body.classList.toggle('light-theme');
 });
